6f63b889fcf9825902748523a2d4363c4c3f4109
SakshiShingala26 <162994954+SakshiShingala26@users.noreply.github.com>
2024-07-10 22:02:29 +0530

    news.py

--- NEWS/news.py
+++ NEWS/news.py
@@ -8,7 +8,7 @@ import requests
 import json
 
 query = input("What type of news you want to receive :: ") # get type of news from user as input 
-r = requests.get(f'https://newsapi.org/v2/everything?q={query}&from=2024-06-10&sortBy=publishedAt&apiKey=d515bbdf565d440ea94376cc7f53bd95') # artical link
+r = requests.get(f'https://newsapi.org/v2/everything?q={query}&from=2024-06-10&sortBy=publishedAt&apiKey=d515bbdf565d440ea94376cc7f53bd95') # artical link from news api
 
 news = json.loads(r.text)
 # print(news , type(news))
